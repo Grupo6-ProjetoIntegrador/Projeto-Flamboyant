9ed38afdabd2213e8de82880ff4358db4b5bce7a
Matheus-slvmr <135768933+Matheus-slvmr@users.noreply.github.com>
2026-06-24 19:57:24 -0300

    Revert "Update 000004_add_token_usuario.up.sql"
    This reverts commit 5d9e8b9181047ab842fa4dd78a939401af93c627.

--- API/migrations/000004_add_token_usuario.up.sql
+++ API/migrations/000004_add_token_usuario.up.sql
@@ -1,5 +1,5 @@
-ALTER TABLE "Usuario" 
-  ADD COLUMN IF NOT EXISTS token_ativo_u     VARCHAR(500),
-  ADD COLUMN IF NOT EXISTS token_expira_em_u TIMESTAMP;
+ALTER TABLE "Usuario"
+  ADD COLUMN token_ativo_u     VARCHAR(500),
+  ADD COLUMN token_expira_em_u TIMESTAMP;
 
-CREATE INDEX IF NOT EXISTS idx_usuario_token ON "Usuario"(token_ativo_u);
+CREATE INDEX idx_usuario_token ON "Usuario"(token_ativo_u);
